12c51bfd97f5f622d23422190dc6fe652fa77779
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2023-08-08 19:37:48 +0700

    có idtoken

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Auth, onAuthStateChanged, signOut } from '@angular/fire/auth';
 import { BehaviorSubject } from 'rxjs';
 import { UserInfor } from '../models/user-info';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   userInfor: BehaviorSubject<UserInfor | null>;
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth, private http: HttpClient) {
     this.userInfor = new BehaviorSubject<UserInfor | null>({
       id: 'id-001',
       name: 'John Doe',
@@ -17,7 +18,7 @@ export class UserService {
     } as UserInfor);
     onAuthStateChanged(
       this.auth,
-      (user) => {
+      async (user) => {
         console.log(user);
         if (user) {
           this.userInfor.next({
@@ -26,6 +27,9 @@ export class UserService {
             email: user.email,
             avatarUrl: user.photoURL,
           } as UserInfor);
+          let idToken = await user!.getIdToken(true);
+          console.log(idToken);
+          this.sendMessage(idToken);
         } else {
           this.userInfor.next(null);
         }
@@ -35,6 +39,20 @@ export class UserService {
       }
     );
   }
+
+  sendMessage(idToken: string) {
+    console.log(idToken);
+    this.http
+      .get('http://localhost:3000', {
+        headers: new HttpHeaders({
+          Authorization: ` ${idToken}`,
+        }),
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
   async logout() {
     await signOut(this.auth);
   }
